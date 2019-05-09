50215bcefb55b4fa2204340782f4afddf9ce9e4e
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2019-05-09 17:58:11 -0400

    add seeds to pre-populate 3 models

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+records = open('db/seed_data.json') do |f|
+  JSON.load(f)
+end
+
+papersxref = {}
+
+# have to create all the papers first
+records.each do |record|
+  if record['model'] == 'papers.newspaper'
+    fields = record['fields']
+    newPaper = Newspaper.create!(name: fields['name'] )
+    # cross-reference old ID with newly created ID
+    papersxref[record['pk']] = newPaper.id
+  end
+end
+
+puts "Created #{Newspaper.all.count} newspaper records."
+
+records.each do |record|
+  if record['model'] == 'papers.townnewssite'
+    fields = record['fields']
+    # get new id from old ID
+    newspaper_id = papersxref[fields['paper']]
+    TownnewsSite.create!(name: fields['name'], URL: fields['URL'], newspaper_id: newspaper_id )
+  end
+end
+
+puts "Created #{TownnewsSite.all.count} BLOX site records."
+
+records.each do |record|
+  if record['model'] == 'utl_files.application'
+    Application.create!(name: record['fields']['name'])
+  end
+end
+
+puts "Created #{Application.all.count} BLOX application records."
